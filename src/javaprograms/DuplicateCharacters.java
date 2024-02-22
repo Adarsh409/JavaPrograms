02a8a711fb7d50d7c3890d2e3a5276c30763a295
Adarsh <adarshkariyadan.143@gmail.com>
2024-02-23 01:08:35 +0530

    Dup chars commit

---
+++ src/javaprograms/DuplicateCharacters.java
@@ -0,0 +1,30 @@
+package javaprograms;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class DuplicateCharacters {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter String: ");
+		String str = sc.next().trim().toLowerCase();
+		HashMap<Character,Integer> hashMap = new HashMap<Character,Integer>();
+		for(int i =0;i<str.length();i++) {
+			if(hashMap.containsKey(str.charAt(i))) {
+				hashMap.put(str.charAt(i), hashMap.get(str.charAt(i))+1);
+			}
+			else {
+				hashMap.put(str.charAt(i), 1);
+			}
+		}
+		System.out.print("Duplicates found are: ");
+		for(Object obj:hashMap.keySet()) {
+			if(hashMap.get(obj)>1) {
+				System.out.print(obj+" ");
+			}
+		}
+
+	}
+
+}

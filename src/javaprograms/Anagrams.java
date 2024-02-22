430f47a786989524b2c4803acf85f9b42925716d
Adarsh <adarshkariyadan.143@gmail.com>
2024-02-23 00:44:29 +0530

    Anagrams commit

---
+++ src/javaprograms/Anagrams.java
@@ -0,0 +1,45 @@
+package javaprograms;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagrams {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter String 1: ");
+		String str1 = sc.next().trim();
+		System.out.println("Enter String 2: ");
+		String str2 = sc.next().trim();
+		if(str1.length() != str2.length()) {
+			System.out.println("Strings are not anagrams");
+		}
+		else {
+			if(areAnagrams(str1.toCharArray(),str2.toCharArray())) {
+				System.out.println("Strings are anagrams");
+			}
+			else {
+				System.out.println("Strings are not anagrams");
+			}
+		}
+		
+	}
+	
+	public static boolean areAnagrams(char[] str1Array,char[] str2Array) {
+		boolean flag = false ;
+		Arrays.sort(str1Array);
+		Arrays.sort(str2Array);
+		for(int i=0;i<str1Array.length;i++) {
+			if(str1Array[i] != str2Array[i]) {
+				flag = false;
+				break;
+			}
+			else {
+				flag = true;
+			}
+		}
+		return flag;
+	}
+	
+	
+}
